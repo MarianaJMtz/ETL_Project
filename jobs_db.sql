cd9d83ca1249920137eb314311658ae6a4637c29
mreyes9406 <mreyes9406@gmail.com>
2019-02-21 20:35:54 -0600

    Modified jobs_db.sql

--- jobs_db.sql
+++ jobs_db.sql
@@ -1,37 +1,39 @@
 DROP DATABASE IF EXISTS public_jobs_db;
-
 CREATE DATABASE public_jobs_db;
-
 USE public_jobs_db;
 
 /*table_1 in Concursos_ET_Process.ipynb*/
+DROP TABLE IF EXISTS job_applications;
 CREATE TABLE job_applications(
     ejercicio INT,
     id_ramo INT,
-    nivel_salarial VARCHAR(50),
-    estatus VARCHAR(30),
+    nivel_salarial VARCHAR,
+    estatus VARCHAR,
     solicitudes_total INT,
     porcentaje_aceptacion FLOAT
 );
 
 /*table_2 in Concursos_ET_Process.ipynb*/
+DROP TABLE IF EXISTS salaries;
 CREATE TABLE salaries(
     ejercicio INT,
     id_ramo INT,
-    nivel_salarial VARCHAR(50),
+    nivel_salarial VARCHAR,
     remuneracion_bruta FLOAT
 );
 
 /*mean_satisfaction in ECCO_PROM_TABLA.ipynb*/
-CREATE TABLE mean_satisfaccion(
-    ramo INT,
+DROP TABLE IF EXISTS mean_satisfaction;
+CREATE TABLE mean_satisfaction(
+    id_ramo INT,
     anyo INT,
     promedio_ecco FLOAT
 );
 
 /*auditorias_sanciones in AUDITORIAS-SANCIONES_TABLA.ipynb*/
+DROP TABLE IF EXISTS auditorias_sanciones;
 CREATE TABLE autitorias_sanciones(
-    ramo INT,
+    id_ramo INT,
     ejercicio INT,
     auditorias INT,
     sanciones INT,
@@ -39,9 +41,10 @@ CREATE TABLE autitorias_sanciones(
 );
 
 /*ramo_description in DESCRIPCIONES_TABLA.ipynb*/
+DROP TABLE IF EXISTS ramo_description;
 CREATE TABLE ramo_description(
-    ramo INT,
-    ramo_descripcion VARCHAR(30)
+    id_ramo INT,
+    ramo_descripcion VARCHAR
 );
 
 
